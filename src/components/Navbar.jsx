1ce8b61137645bf7fe3379494ee93fcebbbc4c2b
Surya Srivastava <suryasrivastava724@gmail.com>
2022-09-23 09:19:45 +0530

    added rel in anchor tags for target blank

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,22 +77,22 @@ const Navbar = () => {
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0' >
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a className='flex justify-between items-center w-full text-gray-300' target="_blank" href="https://www.linkedin.com/in/surya-srivastava-81a6b7176/" >
+                    <a className='flex justify-between items-center w-full text-gray-300' rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/surya-srivastava-81a6b7176/" >
                         LinkedIn <FaLinkedin size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' target="_blank" href="https://github.com/Surya9263">
+                    <a className='flex justify-between items-center w-full text-gray-300' rel="noreferrer" target="_blank" href="https://github.com/Surya9263">
                         GitHub <FaGithub size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' target="_blank" href="mailto:suryasrivastava724@gmail.com">
+                    <a className='flex justify-between items-center w-full text-gray-300' rel="noreferrer" target="_blank" href="mailto:suryasrivastava724@gmail.com">
                         Email <HiOutlineMail size={30} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a href={pdf} download className='flex justify-between items-center w-full text-gray-300' target="_blank">
+                    <a href={pdf} download className='flex justify-between items-center w-full text-gray-300' rel="noreferrer" target="_blank">
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
